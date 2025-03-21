04431372cef96e51f147c4c3201f1bce890f2a7b
Angela <avenable11@ivytech.edu>
2025-03-20 21:36:53 -0400

    unit test with lecture activity

---
+++ 03_20_25/unit test/tests.cpp
@@ -0,0 +1,34 @@
+#include <gtest/gtest.h>
+#include "donut.h"
+
+// lecture activity create a test for the topping setter, and drizzle setter
+
+TEST(donutTest, constructorAndGetterTest)
+{
+    EXPECT_NO_THROW(Donut d("Chocolate", "Peanuts", "Peanut Butter"));
+    EXPECT_NO_THROW(Donut d2("chocolate", "peanuts", "peanut butter"));
+    Donut d("Chocolate", "Peanuts", "Peanut Butter");
+    EXPECT_EQ(d.getIcing(), Donut::CHOCOLATEICING);
+    EXPECT_EQ(d.getTopping(), Donut::PEANUTS);
+    EXPECT_EQ(d.getDrizzle(), Donut::PEANUTBUTTER);
+    EXPECT_ANY_THROW(Donut d("coco", "nut", "stuff"));
+}
+
+TEST(donutTest, icingSetterTest)
+{
+    Donut d("Chocolate", "Peanuts", "Peanut Butter");
+    EXPECT_ANY_THROW(d.setIcing("coco"));
+    EXPECT_NO_THROW(d.setIcing("MAplE"));
+    EXPECT_EQ(d.getIcing(), Donut::MAPLE);
+}
+
+extern "C" int startTest(int x, char **y)
+{
+    testing::InitGoogleTest(&x, y);
+    int code = RUN_ALL_TESTS();
+    return code;
+}
+extern "C" int __wrap_main(int x, char **y)
+{
+    return startTest(x, y);
+}
