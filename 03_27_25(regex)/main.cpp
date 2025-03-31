11fe479753aba1982593842ef879f27e1e7758c9
camelane21 <157440747+camelane21@users.noreply.github.com>
2025-03-31 14:57:37 -0400

    Update main.cpp

--- 03_27_25(regex)/main.cpp
+++ 03_27_25(regex)/main.cpp
@@ -9,18 +9,20 @@ int main()
 {
     // lecture activity create a regular expression to divide up a tab-separated string
     // change the loop to parse a tab-separated string.
-    std::regex commaStr{R"(((?:\w+\s*)+(?=,|$)))"};
-    std::regex tabStr{};
+    std::regex tabStr{R"([^\t]+)"};
     std::ifstream in("test.txt");
+    
     while (!in.eof() && in.is_open())
     {
         std::string line;
         std::getline(in, line);
         const std::sregex_iterator end;
-        for (std::sregex_iterator iter{std::cbegin(line), std::cend(line), commaStr}; iter != end; ++iter)
+
+        for (std::sregex_iterator iter{std::cbegin(line), std::cend(line), tabStr}; iter != end; ++iter)
         {
             std::cout << (*iter)[0].str() << std::endl;
         }
     }
+
     return 0;
 }
