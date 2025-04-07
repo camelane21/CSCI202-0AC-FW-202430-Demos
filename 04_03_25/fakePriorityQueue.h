70f630ad66374e22f58489448364594bbbfd8830
camelane21 <157440747+camelane21@users.noreply.github.com>
2025-04-07 15:24:20 -0400

    Update fakePriorityQueue.h

--- 04_03_25/fakePriorityQueue.h
+++ 04_03_25/fakePriorityQueue.h
@@ -13,19 +13,45 @@ public:
 template <class t>
 void fakePriorityQueue<t>::enqueue(const t &queueElement)
 {
-    node<t> *newNode;
-    newNode = new node<t>;
+    node<t> *newNode = new node<t>;
     newNode->data = new t(queueElement);
     newNode->link = nullptr;
+
     if (this->isEmptyQueue())
     {
-        this->queueFront = newNode;
-        this->queueRear = newNode;
+        // Queue is empty, insert as first and last
+        this->queueFront = this->queueRear = newNode;
     }
     else
     {
-        this->queueRear->link = newNode;
-        this->queueRear = newNode;
+        node<t> *current = this->queueFront;
+        node<t> *trailCurrent = nullptr;
+
+        // Traverse to find the correct insert location (ties go after)
+        while (current != nullptr && *(current->data) <= queueElement)
+        {
+            trailCurrent = current;
+            current = current->link;
+        }
+
+        if (trailCurrent == nullptr)
+        {
+            // Insert at the front
+            newNode->link = this->queueFront;
+            this->queueFront = newNode;
+        }
+        else
+        {
+            // Insert after trailCurrent
+            trailCurrent->link = newNode;
+            newNode->link = current;
+
+            if (current == nullptr)
+            {
+                // Inserted at the end, update rear
+                this->queueRear = newNode;
+            }
+        }
     }
 }
 
