316552c896fc3f0a95f6b81dd40c540e7ac13e60
camelane21 <157440747+camelane21@users.noreply.github.com>
2025-04-26 21:54:23 -0400

    Update graph.h

--- 04_22_25/graph.h
+++ 04_22_25/graph.h
@@ -34,5 +34,22 @@ private:
     void dft(int v, bool visited[], std::string &output);
     void copyGraph(const graphType &);
 };
+inline bool graphType::connected(int vertex1, int vertex2)
+{
+    if (vertex1 < 0 || vertex1 >= graph.size() || vertex2 < 0 || vertex2 >= graph.size())
+    {
+        throw std::out_of_range("Vertex index out of range.");
+    }
 
+    nodeType<int> *current = graph[vertex1].first;
+    while (current != nullptr)
+    {
+        if (current->info == vertex2)
+        {
+            return true;
+        }
+        current = current->link;
+    }
+    return false;
+}
 #endif
